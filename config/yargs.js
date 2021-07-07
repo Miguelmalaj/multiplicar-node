f1df2200f311d5f601fdb532ee51b3e1b64cb0f8
hsmiguelhs@gmail.com <hsmiguelhs@gmail.com>
2021-07-06 19:27:32 -0600

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,22 @@
+const opt = {
+    base: {
+        demand: true,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', opt)
+    .command('crear', 'Generate a file with a multiplicate table', opt)
+    .help()
+    .argv;
+
+
+
+module.exports = {
+    argv: argv
+}
